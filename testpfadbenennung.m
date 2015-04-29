af1b681bbcae99da2c69e79afd85f89d587d0bee
Felicia Primadita <felicia.primadita@loungepad.de>
2015-04-29 12:44:47 +0200

    veränderung

--- testpfadbenennung.m
+++ testpfadbenennung.m
@@ -7,12 +7,40 @@ opendir = char(opendir);
 pfad= [];
 
 for kk = 1:length(gh)
-    pfad(kk,:) = [opendir '\' hh(kk,:)];
+    pfad(kk,:) = ['\' hh(kk,:) '.wav'];
 end
 
-fname = char(pfad) %fname beinhaltet leider noch Leerzeichen :(
+fname = char(pfad); 
+fname = cellstr(fname);
+fname = strrep(fname,' ','');
+fnameneu = char(fname);
 
-% for ll=1:length(gh)
-%     getaudio(ll)=uigetfile('fname(1,:).wav');
-%     playaudio=play(getaudio(ll));
+pfadcomplette= [];
+for ll=1: length(fname)
+    pfadcomplette(ll,:) = [opendir fnameneu(ll,:)];
+end
+
+pfad1 = char(pfadcomplette);
+pfad1 = cellstr(pfad1);
+
+timit_text = uicontrol('style','listbox',...
+                       'string',pfad1,...
+                       'callback',@playselectedaudio,...
+                       'position',[100 200 300 100]);
+% timit_text.String = pfad1;
+
+% for ll=1:length(fname)
+%    timit_text.String = {{timit_text.String} {fname{ll}}}; 
 % end
+
+chosen_audiox = pfad1{timit_text.Value};
+[y,fs]= audioread(chosen_audiox);
+play_button = uicontrol('style','pushbutton',...
+                        'string','Play',...
+                        'callback','sound(y,fs)',...
+                        'position', [400 200 50 50]);
+
+% audio = pfad1{1};
+% [y,fs] = audioread(audio);
+% sound(y,fs)
+% 
