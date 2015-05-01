36c0ead5499a9ff4d76fc41d32ce407112b2930d
JanTjorbenArning <jan_tjorben_arning@t-online.de>
2015-05-01 12:25:19 +0200

    jetzt läufts mit den pfaden und dem Play

--- playselectedaudio.m
+++ playselectedaudio.m
@@ -2,6 +2,6 @@ function playselectedaudio(handle,event)
     audio = get(handle, 'string');
     chosen_index = get(handle, 'value');
     chosen_audio = audio{chosen_index}; %pass auf: string ist ein cell-array
-    [y,fs]=audioread(chosen_audio);
+    [y,fs]=wavread(chosen_audio); % damit Linux und Apple auch die Datei Abspiel können
     sound(y,fs)
 end
