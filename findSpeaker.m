6bf134e6724676fb9036061e82518bc95df9a6ea
HelenKopter <h3l1k0pt3r61@gmail.com>
2015-05-01 12:05:43 +0200

    neuer kopf, verenglischt

--- findSpeaker.m
+++ findSpeaker.m
@@ -1,4 +1,16 @@
-function [findPathID] = findSpeaker(suchwort)
+function [findPathID] = findSpeaker(lookForSpeaker)
+% function to find filenames of files contenting data recorded by speaker
+% (you want to look for)
+% Usage: findSpeaker(suchwort)
+% Input parameter:
+%       suchwort:     ID of speaker you want to look for
+% Output parameter:
+%       filenames of files contenting data recorded by speaker
+%       (you want to look for)
+%--------------------------------------------------------------------------
+% Example: pathSpeaker = findSpeaker('cpm0');
+% 
+
 %index_k=[];                         % Vorallokieren des Indexvektors der Wortposition im Satz
 index_r=[];                         % Vorallokieren des Indexvektors der Satzpostion, in dem Wort enthalten
 d=fopen('TIMIT MIT/allfilelist.txt');% Oeffnen der Textdatei
@@ -7,9 +19,9 @@ if d~=-1                            % Sicherheitsabfrage, ob Textdatei existiert
    data=textscan(d,'%s'); %Zerlegen der Saetze in Woerter 
    
    for rr=1:160                 % Ablaufen jedes inneren Cell-Arrays
-       wort=data{1}(rr);      % Auslesen der DAten aus Cell-Array in Vektor
-       wort=char(wort);        % Umwandeln Cell-Array-Eintrag in String
-       s=regexp(wort,suchwort);% Vergleich Suchwort/Wort
+       speaker=data{1}(rr);      % Auslesen der DAten aus Cell-Array in Vektor
+       speaker=char(speaker);        % Umwandeln Cell-Array-Eintrag in String
+       s=regexp(speaker,lookForSpeaker);% Vergleich Suchwort/Wort
             
        if s==6;
        %index_k=[index_k; kk-1]; % Extrahieren der Indizes, wo Suchwort gleich Wort
