cfd5a8db29fd5af521ca017d63c7abf6572a7c3c
Felicia Primadita <felicia.primadita@loungepad.de>
2015-05-05 08:54:13 +0200

    function's details added

--- playTIMIT.m
+++ playTIMIT.m
@@ -1,12 +1,31 @@
 function [List]= playTIMIT()
-    % die Idee von playTIMIT ist hier die einzelne Funktion zu kombinieren.
-    % Dafür werden die Funktionen vorher einzeln aufgerufen. Am Ende mit
-    % 'ismember' alles vergleichen, damit wir nur die überschneidenden
-    % Dateien bekommen.
+% function to find and playback filename of files contenting data recorded 
+% by combination of speaker,sentence,word and phoneme(you want to look for).
+% Usage: playTIMIT()
+% Input parameters: will be asked while executing playTIMIT and have to be
+% written as a string. (see example) You are allowed to leave some
+% parameters empty by pressing ENTER/RETURN. Note: there should be at least
+% one parameter filled in.
+%       speaker: speaker's ID you are looking for
+%       sentence: sentence you are looking for -- forget about apostrophes
+%       word: word you are looking for -- include apostrophes
+%       phoneme: phoneme you are looking for.
+
+% Output parameter:
+%       listbox with filenames of files contenting data recorded, which are
+%       true for all input parameter filled in. Click on one name, then the
+%       *.wav-data will be automatically.
+%--------------------------------------------------------------------------
+% Example: Type the speaker ID, or press ENTER to continue: 'cmp0'
+%          Type the sentence,or press ENTER to continue: 'dont ask me to
+%          carry an oily rag like that' 
+%          Type the word, or press ENTER to continue: 'don''t'
+%          Type the phoneme, or ENTER to continue: 'sh'
     
-    opendir = pwd;
+    opendir = pwd; %open current directory
     opendir = [opendir '\TIMIT MIT'];
     disp('All inputs have to be written as a string (in apostrophes).')
+    % now the inputs are asked
     speaker = input('Type the speaker ID, or press ENTER to continue: ');
     
     sentence = input('Type the sentence,or press ENTER to continue: ');
@@ -15,25 +34,26 @@ function [List]= playTIMIT()
    
     phoneme = input('Type the phoneme, or ENTER to continue: ');
     
-    List = master(speaker, sentence, word, phoneme);
+    
+    List = master(speaker, sentence, word, phoneme);        %put in inputs in the master function
 
     endlist = char(List);
     attachwav= [];
     attach_opendir = [];
     
     for kk = 1:length(List)
-        attachwav(kk,:) = ['\' endlist(kk,:) '.wav'];
+        attachwav(kk,:) = ['\' endlist(kk,:) '.wav']; %wav an dateinamen haengen
     end
     if 1==isempty(endlist)
        disp('No matches found.')
     else
     fname = char(attachwav); 
     fname = cellstr(fname);
-    fname = strrep(fname,' ','');
+    fname = strrep(fname,' ',''); % leerzeichen zwischen Dateinamen und *.wav loeschen.
     fnameneu = char(fname);
     
     for ll=1: length(fname)
-        attach_opendir(ll,:) = [opendir fnameneu(ll,:)];
+        attach_opendir(ll,:) = [opendir fnameneu(ll,:)]; % komplette Namen von Pfad bekommen
     end
 
     pfadcomplette = char(attach_opendir);
